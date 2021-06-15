4876d00acc82971db8dad2c7159a946413cb24e0
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-15 14:56:14 +0630

    integer anf float

---
+++ intro_to_python/01-datatype-and-operator/integer_and_float.py
@@ -0,0 +1,14 @@
+# Integer and Float
+
+# int for integer values
+# floar for decimal or floating point values
+
+# type built-in function
+
+print(type(4.0))
+print(type(4))
+
+# convert type
+
+print(int(4.5))
+print(float(4))
