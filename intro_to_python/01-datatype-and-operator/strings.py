40a367acab578a801a640e45c7598ff89efa0ecd
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-15 15:49:49 +0630

    strings

---
+++ intro_to_python/01-datatype-and-operator/strings.py
@@ -0,0 +1,22 @@
+# String
+
+# String for sequence of charactes(letters,numbers,spaces and symbols)
+
+# String with double quote
+print("Hello")
+# String with single quote
+print('Hello')
+
+# quote string
+print('"Myanmar Spring Revolution"')
+
+# string combine
+first = "hello "
+second = "world!"
+print(first + second)
+
+# string multiplication
+print(first * 5)
+
+# string length
+print(len(first))
