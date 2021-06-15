3e099a8ecb7b948faf7a4ab343a3daa8c95137a5
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-15 14:57:03 +0630

    arithmetic operator

---
+++ intro_to_python/01-datatype-and-operator/arithmetic_operator.py
@@ -0,0 +1,23 @@
+# addition
+print(3+4)
+
+# subtraction
+print(4-3)
+
+# multiplication
+print(4*3)
+
+# division
+print(4/3)
+
+# exponentiation
+print(3**4)
+
+# caret
+print(2^3)
+
+# modulo
+print(4%3)
+
+# integer division
+print(7//2)
