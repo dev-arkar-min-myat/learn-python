6c9dcf7d7fd5edc19ca32cc51f51001840653f03
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-15 15:49:07 +0630

    string methods

---
+++ intro_to_python/01-datatype-and-operator/string_methods.py
@@ -0,0 +1,10 @@
+# String Method
+
+# title
+print("aung san su kyi".title())
+
+# islower
+print("aung san su kyi".islower())
+
+# count
+print("one fist,two fist,three fist,four fist".count("fish"))
