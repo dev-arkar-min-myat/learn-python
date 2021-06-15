429bd566025ecd63247f285de5e23268e8d9ec42
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-15 15:09:49 +0630

    boolean,logical,comparison

---
+++ intro_to_python/01-datatype-and-operator/boolean_comparison_logical.py
@@ -0,0 +1,28 @@
+# Boolean Comparison and Logical Operator
+
+# Boolean True or False
+
+sun_is_red = True
+ocean_is_red = False
+
+# Use comparison operator to evalute Boolean values
+
+x = 43 > 42
+print(x)
+
+# Comparison Operator
+# > greater than
+# >= greater than equal
+# < less than
+# <= less than equal
+# == eqaul
+# != not equal
+
+# Logical Operator
+# and if both sides are true
+# or if one side is true
+# not inverse bool values
+
+print(True and True)
+print(True or False)
+print(not True)
