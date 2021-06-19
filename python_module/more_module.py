2d98a9e23fb7ad64b87913e0b3c2d5db38b393ec
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-19 19:32:40 +0630

    more module

---
+++ python_module/more_module.py
@@ -0,0 +1,17 @@
+# More Module
+
+from fibo import fib,fib2
+
+fib(500)
+
+from fibo import *
+
+fib(500)
+
+import fibo as fibonacci
+
+fibonacci.fib(500)
+
+from fibo import fib as fibonacci
+
+fibonacci(500)
