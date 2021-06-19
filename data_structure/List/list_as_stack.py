1995ee781a6edab48ea66be3cbf3dd4c5bf91eb0
wang-ou-peng <dev.rkarminmyat@gmail.com>
2021-06-19 07:40:43 +0630

    list as stack

---
+++ data_structure/List/list_as_stack.py
@@ -0,0 +1,23 @@
+# List as Stacks
+
+>>> stack = [2,3,4]
+>>> stack
+[2, 3, 4]
+>>> stack.append(5)
+>>> stack
+[2, 3, 4, 5]
+>>> stack.append(7)
+>>> stack
+[2, 3, 4, 5, 7]
+>>> stack.pop()
+7
+>>> stack
+[2, 3, 4, 5]
+>>> stack.pop()
+5
+>>> stack
+[2, 3, 4]
+>>> stack.pop()
+4
+>>> stack
+[2, 3]
